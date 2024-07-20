d641661d9d7e0db87c0e60c59466934682b9eecc
pavan92m <pavan92m@gmail.com>
2024-07-20 10:53:30 +0530

    basic leaning

---
+++ src/com/interview/java8/DuplicateNumbers.java
@@ -0,0 +1,15 @@
+package com.interview.java8;
+
+import java.util.*;
+
+public class DuplicateNumbers {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,2,7,8,3,2,1,5);
+        Set<Integer> duplicate =  new HashSet<>();
+        //print duplicate numbers
+        numbers.stream().filter(n->Collections.frequency(numbers,n)>1).distinct().toList().forEach(System.out::println);
+        //print duplicate numbers
+        List<Integer> newList = numbers.stream().filter(n->!duplicate.add(n)).toList();
+        System.out.println(newList);
+    }
+}
---
+++ src/com/interview/java8/SecondLargestNumber.java
@@ -0,0 +1,13 @@
+package com.interview.java8;
+
+import java.util.*;
+
+public class SecondLargestNumber {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(12,34,56,23,4,56,34,44,55,43,55);
+        List<Integer> numbersNew = numbers.stream().sorted().distinct().toList();
+        System.out.println(numbersNew);
+        int secondLargestIndex = numbersNew.size()-2;
+        System.out.println(numbersNew.get(secondLargestIndex));
+    }
+}
