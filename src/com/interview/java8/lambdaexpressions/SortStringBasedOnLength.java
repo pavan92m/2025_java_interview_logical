82bc10306445830df028856f79fd1f039bb2548d
pavan92m <pavan92m@gmail.com>
2024-08-10 14:02:34 +0530

    Java 8 basic coding

---
+++ src/com/interview/java8/lambdaexpressions/SortStringBasedOnLength.java
@@ -0,0 +1,25 @@
+package com.interview.java8.lambdaexpressions;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortStringBasedOnLength {
+    public static void main(String[] args) {
+        List<String> foodsList = Arrays.asList("apple", "banana", "cherry", "Milk");
+        List<String> sortedFoodList = foodsList.stream().sorted(Comparator.comparing(String::length)).toList();
+        System.out.println(sortedFoodList);
+        List<String> sortedFoodList2 = foodsList.stream().sorted(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length()>o2.length())
+                    return 1;
+                else if (o1.length()<o2.length()) {
+                    return -1;
+                }
+                return 0;
+            }
+        }).toList();
+        System.out.println(sortedFoodList2);
+    }
+}
